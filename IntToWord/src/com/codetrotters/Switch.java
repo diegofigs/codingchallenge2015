fced08203f85a0e8e3f09e7b2df8736f8b9218db
Diego Figueroa <diego.fv94@gmail.com>
2015-11-10 21:47:23 -0500

    added implementation
    Implemented int_to_word method and commented for readability.

--- IntToWord/src/com/codetrotters/Switch.java
+++ IntToWord/src/com/codetrotters/Switch.java
@@ -1,5 +1,8 @@
 package com.codetrotters;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by diegofigs on 11/10/15.
  */
@@ -45,6 +48,9 @@ public class Switch {
     };
 
     public static String int_to_word(int number){
+        // Queue that acts as a buffer for words
+        Queue<String> wordQueue = new LinkedList<String>();
+        // Entered number in String format
         String numberString = Integer.toString(number);
         int length = numberString.length();
 
@@ -59,26 +65,44 @@ public class Switch {
             // Third position
             if(position % 3 == 0){
                 // Add digit word
-
+                wordQueue.add(NUMBERS[digit]);
                 // Add hundred
-
+                wordQueue.add("hundred");
             }
 
             // Second position
             else if(position == 8 || position == 5 || position == 2){
-                // Check if higher than 1
-
-                // Else, look at next number for correct teen number
+                // If higher than 1, add digit by ten word
+                if(digit > 1)
+                    wordQueue.add(BYTENS[digit]);
+                // Else, look at next number for correct teen number word
+                else {
+                    String teenNumber = numberString.substring(i,i+2);
+                    int teenDigit = Character.getNumericValue(teenNumber.charAt(1));
+                    wordQueue.add(TEENS[teenDigit]);
+                }
             }
-            
+
             // First position
             else {
                 // Check if previous number is higher than 1
-
+                if(Character.getNumericValue(numberString.charAt(i-1)) > 1){
+                    // Add digit word if not zero
+                    if(digit != 0)
+                        wordQueue.add(NUMBERS[digit]);
+                }
                 // Add million or thousand if necessary
+                if(position == 7)
+                    wordQueue.add("million");
+                else if(position == 4)
+                    wordQueue.add("thousand");
             }
         }
-
-        return null;
+        // Fill numberString with words
+        numberString = "";
+        for(String word: wordQueue) {
+            numberString += word + " ";
+        }
+        return numberString.trim();
     }
 }
